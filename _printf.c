0324929e5cc95413c7f6030ad798c5322d560bb5
Precious <eseprecious934@gmail.com>
2023-10-17 08:06:25 -0700

    compiled

--- _printf.c
+++ _printf.c
@@ -56,6 +56,7 @@ int _printf(const char *format, ...)
 	while (*format != '\0')
 	{
 		if (*format == '%')
+		{
 			format++;
 			switch (*format)
 			{
@@ -76,6 +77,7 @@ int _printf(const char *format, ...)
 					}
 					break;
 			}
+		}
 		else
 		{
 			write(1, format, 1);
